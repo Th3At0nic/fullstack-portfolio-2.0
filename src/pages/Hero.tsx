88876ef5b480b959fdcf25b5f5083718b352d3fb
Th3At0nic <islammdrahatul@gmai.com>
2025-06-03 00:25:46 +0600

    blog page done, responvice

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button } from "antd";
+import { Button, Spin } from "antd";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   useGetProfileDataQuery,
@@ -7,9 +7,9 @@ import {
 } from "../redux/features/data/dataManagement.api";
 
 const Hero = () => {
-  const { data: profileData } = useGetProfileDataQuery(undefined);
+  const { data: profileData, isLoading: isProfileDataLoading } =
+    useGetProfileDataQuery(undefined);
   const { data: resume } = useGetResumeQuery(undefined);
-  //   const [showResume, setShowResume] = useState(false);
   const [currentBioIndex, setCurrentBioIndex] = useState(0);
 
   const user = profileData?.data?.[0];
@@ -23,8 +23,19 @@ const Hero = () => {
     return () => clearInterval(interval);
   }, [bioList.length]);
 
+  if (isProfileDataLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-[60vh]">
+        <Spin size="large" />
+      </div>
+    );
+  }
+
   return (
-    <div className="text-gray-800 flex flex-col items-center" style={{margin: "5% 0"}}>
+    <div
+      className="text-gray-800 flex flex-col items-center"
+      style={{ margin: "5% 0" }}
+    >
       {/* Profile Image */}
       <motion.img
         src={user?.avatarUrl}
@@ -70,7 +81,10 @@ const Hero = () => {
       </motion.p>
 
       {/* Buttons */}
-      <div className="flex flex-col sm:flex-row gap-4" style={{margin: "15px 0"}}>
+      <div
+        className="flex flex-col sm:flex-row gap-4"
+        style={{ margin: "15px 0" }}
+      >
         {resume?.data[0]?.resumeUrl && (
           <Button
             type="primary"
